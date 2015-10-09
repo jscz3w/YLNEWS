267e3c493d6b22b488c5dbbac90d61520ae0d1a4
jscz3w <jscz3w@126.com>
2015-10-09 17:00:18 +0800

    ABSQLite 1.20

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 target "亿利MIS" do
 pod 'AFNetworking', '~> 2.6.0'
+pod 'ABSQLite', '~> 1.2.0'
 end
---
+++ Pods/Target Support Files/ABSQLite/ABSQLite-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ABSQLite : NSObject
+@end
+@implementation PodsDummy_ABSQLite
+@end
